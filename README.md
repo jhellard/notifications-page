bc11e7c383193f5df18d9ab46a2fe25fc2456503
Joshua Hellard <67675776+jhellard@users.noreply.github.com>
2022-09-16 13:33:26 -0400

    v1 Complete
    Challenge completed for Frontend Mentor

--- README.md
+++ README.md
@@ -1,70 +1,55 @@
-# Getting Started with Create React App
+# Frontend Mentor - Notifications page solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Hello again! 👋
 
-## Available Scripts
+Below is a solution to the [Notifications page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/notifications-page-DqK5QAmKbC). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-In the project directory, you can run:
 
-### `npm start`
+## Table of contents
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Overview
 
-### `npm test`
+### The challenge
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Users should be able to:
 
-### `npm run build`
+- Distinguish between "unread" and "read" notifications ✅
+- Select "Mark all as read" to toggle the visual state of the unread notifications and set the number of unread messages to zero ✅
+- View the optimal layout for the interface depending on their device's screen size ✅
+- See hover and focus states for all interactive elements on the page ✅
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Screenshot
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+![](/src/images/complete-mobile.png)
+![](/src/images/complete-desktop.png)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+### Links
 
-### `npm run eject`
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## My process
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### Built with
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [Sass](https://sass-lang.com/) - For styles
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+## Author
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Website - [Joshua Hellard](https://www.jhellard.com)
+- Frontend Mentor - [@jhellard](https://www.frontendmentor.io/profile/jhellard)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,211 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./SCSS/styles.scss";
+
+const App = () => {
+  const toggle = (e) => {
+    let count = document.getElementById("count");
+    let number = count.innerHTML;
+
+    e.currentTarget.classList.toggle("active");
+    if (e.currentTarget.classList[1] === "active") {
+      number++;
+      count.innerHTML = number;
+    } else {
+      number--;
+      count.innerHTML = number;
+    }
+  };
+
+  const resetCount = () => {
+    let count = document.getElementById("count");
+    count.innerHTML = 0;
+
+    document
+      .querySelectorAll(".active")
+      .forEach((el) => el.classList.toggle("active"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <main className="main">
+        <header className="header">
+          <h1 className="header__notifications">
+            Notifications{" "}
+            <span className="header__count" id="count">
+              {3}
+            </span>
+          </h1>
+          <button className="header__mark" onClick={() => resetCount()}>
+            Mark all as read
+          </button>
+        </header>
+
+        <section className="notifications">
+          <ul className="notifications__list">
+            <li
+              className="notifications__item active"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-mark-webber.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Mark Webber</span>{" "}
+                  reacted to your recent post{" "}
+                  <span className="notifications__post">
+                    My first tournament today!
+                  </span>
+                  <span className="notifications__dot"></span>
+                </p>
+
+                <p className="notifications__time">1m ago</p>
+              </div>
+            </li>
+            <li
+              className="notifications__item active"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-angela-gray.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Angela Gray</span>{" "}
+                  followed you
+                  <span className="notifications__dot"></span>
+                </p>
+                <p className="notifications__time">5m ago</p>
+              </div>
+            </li>
+            <li
+              className="notifications__item active"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-jacob-thompson.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Jacob Thompson</span>{" "}
+                  has joined your group{" "}
+                  <a href="/" className="notifications__club">
+                    Chess Club
+                  </a>
+                  <span className="notifications__dot"></span>
+                </p>
+                <p className="notifications__time">1 day ago</p>
+              </div>
+            </li>
+            <li
+              className="notifications__item"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-rizky-hasanuddin.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Rizky Hasanuddin</span>{" "}
+                  sent you a private message
+                  <span className="notifications__dot"></span>
+                </p>
+                <p className="notifications__time">5 days ago</p>
+                <p className="notifications__private">
+                  Hello, thanks for setting up the Chess Club. I've been a
+                  member for a few weeks now and I'm already having lots of fun
+                  and improving my game.
+                </p>
+              </div>
+            </li>
+            <li
+              className="notifications__item"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-kimberly-smith.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Kimberly Smith</span>{" "}
+                  commented on your picture
+                  <span className="notifications__dot"></span>
+                </p>
+
+                <p className="notifications__time">1 week ago</p>
+              </div>
+              <img
+                className="notifications__image"
+                src={require("./images/image-chess.webp")}
+                alt="User Submitted"
+              />
+            </li>
+            <li
+              className="notifications__item"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-nathan-peterson.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Nathan Peterson</span>{" "}
+                  reacted to your recent post{" "}
+                  <span className="notifications__post">
+                    5 end-game strategies to increase your win rate
+                  </span>
+                  <span className="notifications__dot"></span>
+                </p>
+
+                <p className="notifications__time">2 weeks ago</p>
+              </div>
+            </li>
+            <li
+              className="notifications__item"
+              id="item"
+              onClick={(e) => toggle(e)}
+            >
+              <img
+                className="notifications__avatar"
+                src={require("./images/avatar-anna-kim.webp")}
+                alt="User Avatar"
+              />
+              <div className="notifications__content">
+                <p className="notifications__text">
+                  <span className="notifications__name">Anna Kim</span> left
+                  your group{" "}
+                  <a href="/" className="notifications__club">
+                    Chess Club
+                  </a>
+                  <span className="notifications__dot"></span>
+                </p>
+                <p className="notifications__time">2 weeks ago</p>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
